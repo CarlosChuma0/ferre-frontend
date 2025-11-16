f96bd1e313e4f5e8475e686695dc5afbc912f448
CarlosChuma0 <40726830@fi.unju.edu.ar>
2025-11-16 20:50:32 -0300

    Metodo en app.ts para recolectar navegacion entre rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag: Function;  // <- para que TS conozca gtag()
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Footer, RouterOutlet],
+  imports: [ Header, Footer, RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend-hia');
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-0SEYBSS7CH', {
+          page_path: event.urlAfterRedirects
+          // opcional:
+          // page_title: document.title
+        });
+      });
+  }
 }
